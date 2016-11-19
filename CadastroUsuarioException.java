e0ac4bab82665bc6a0360155a1894e7785c118ea
Alex <alexantonio308@gmail.com>
2016-11-19 21:19:32 -0200

    Update and rename LICENSE.md to CadastroUsuarioException.java

---
+++ CadastroUsuarioException.java
@@ -0,0 +1,15 @@
+package cadusuarios;
+
+public class CadastroUsuarioException extends RuntimeException {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CadastroUsuarioException(String msg){
+		super(msg);
+	}
+
+}
